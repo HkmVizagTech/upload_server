0975dde4f02daa02acb21c4d3fcfcc1759249535
SivaBalajiEevana-12 <eevanasivabalaji@gmail.com>
2026-03-02 15:24:17 +0530

    final

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const connectDB = require('./models/db');
+// const donorRoutes = require('./routes/donorRoutes');
+const multer = require('multer');
+const XLSX = require('xlsx');
+const cors = require('cors');
+const Donor = require('./models/donor');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.use(cors())
+
+app.use(express.json());
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+app.post("/upload-donors", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    // ✅ Read Excel
+    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    let data = XLSX.utils.sheet_to_json(sheet);
+
+    // ✅ Transform
+    data = data.map((row) => ({
+      receiptNumber: row.ReceiptNumber,
+      receiptDate: row.ReceiptDate ? new Date(row.ReceiptDate) : null,
+      amount: Number(row.Amount),
+      enrolledBy: row.EnrolledBy,
+      donorId: row.DonorID,
+      donorName: row.DonorName,
+      mobileNumber: String(row.MobileNumber).trim(),
+      sevaSubCategoryCode: row.SevaSubCategoryCode,
+      type: row.Type,
+    }));
+
+    await Donor.deleteMany({});
+
+    await Donor.insertMany(data);
+
+    res.json({
+      message: "Donor data uploaded successfully",
+      count: data.length,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Upload failed" });
+  }
+});
+
+app.get("/donors-by-mobile/:mobile", async (req, res) => {
+  try {
+    const mobile = req.params.mobile.trim();
+
+    const donors = await Donor.find({
+      mobileNumber: mobile,
+    }).sort({ receiptDate: -1 });
+
+    if (!donors.length) {
+      return res.status(404).json({ message: "No records found" });
+    }
+
+    res.json(donors);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching donors" });
+  }
+});
+
+
+// const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/donor.js
@@ -0,0 +1,23 @@
+// models/Donor.js
+const mongoose = require("mongoose");
+
+const donorSchema = new mongoose.Schema(
+  {
+    receiptNumber: String,
+    receiptDate: Date,
+    amount: Number,
+    enrolledBy: String,
+    donorId: String,
+    donorName: String,
+    mobileNumber: {
+      type: String,
+      required: true,
+      index: true, // 🔥 important for search
+    },
+    sevaSubCategoryCode: String,
+    type: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Donor", donorSchema);
